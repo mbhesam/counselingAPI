6fa127a6001881dd2186c96ea1855038b75f2d7b
mbhesam <m.b.hesam>
2024-02-02 10:06:05 +0330

    Add admin for register application

--- registerAPI/admin.py
+++ registerAPI/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import RegisterModel
 
-# Register your models here.
+@admin.register(RegisterModel)
+class RegisterAdmin(admin.ModelAdmin):
+    empty_value_display = "-خالی-"
+    fields = ['phone_number','sent_sms_at','is_mobile']
