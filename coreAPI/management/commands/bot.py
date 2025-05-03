47393374d2969c9af0b55c45b8485b66f2c5e854
mb <mb@pouyan.ai>
2025-05-03 18:28:44 +0330

    fix proxy

--- coreAPI/management/commands/bot.py
+++ coreAPI/management/commands/bot.py
@@ -1,26 +1,26 @@
 from telegram.ext import ApplicationBuilder, PicklePersistence
 from moshavereBOT.handlers import main_handler
-from moshavereAPI.settings import BOT_TOKEN, REQUEST_TIME_OUT, PROXY_URL
+#from moshavereAPI.settings import BOT_TOKEN, REQUEST_TIME_OUT, PROXY_URL
 from django.core.management.base import BaseCommand, CommandError
 import os
 
 os.environ["DJANGO_ALLOW_ASYNC_UNSAFE"] = "true"
 
 
-class Command(BaseCommand):
-    persistence = PicklePersistence(filepath='conversations/conversations')
-    application = ApplicationBuilder().token(
-        BOT_TOKEN
-    ).get_updates_proxy_url(
-        PROXY_URL
-    ).proxy_url(
-        PROXY_URL
-    ).persistence(persistence).build()
-    application.add_handler(main_handler)
-    application.run_polling(
-        timeout=REQUEST_TIME_OUT,
-        pool_timeout=REQUEST_TIME_OUT,
-        read_timeout=REQUEST_TIME_OUT,
-        write_timeout=REQUEST_TIME_OUT,
-        connect_timeout=REQUEST_TIME_OUT,
-    )
+# class Command(BaseCommand):
+#     persistence = PicklePersistence(filepath='conversations/conversations')
+#     application = ApplicationBuilder().token(
+#         BOT_TOKEN
+#     ).get_updates_proxy_url(
+#         PROXY_URL
+#     ).proxy_url(
+#         PROXY_URL
+#     ).persistence(persistence).build()
+#     application.add_handler(main_handler)
+#     application.run_polling(
+#         timeout=REQUEST_TIME_OUT,
+#         pool_timeout=REQUEST_TIME_OUT,
+#         read_timeout=REQUEST_TIME_OUT,
+#         write_timeout=REQUEST_TIME_OUT,
+#         connect_timeout=REQUEST_TIME_OUT,
+#     )
